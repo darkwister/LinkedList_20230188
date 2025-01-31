f927e71f3dc0ae1bdc283a39a65617ebf6be81f2
DarkWister <darkwistercrak17@gmail.com>
2025-01-31 15:46:28 -0400

    Funcional

---
+++ Lista.py
@@ -0,0 +1,99 @@
+import Nodo
+
+class List:
+    def __init__(self):
+        self.inicio = None
+
+    #Instacia un nuevo nodo al inicio de la lista
+    def insertar_inicio(self, item):
+        nuevo_nodo = Nodo.Nodo(item)
+        nuevo_nodo.next = self.inicio
+        self.inicio = nuevo_nodo
+
+    #Busca el final de la lista mediante un puntero, y luego agrega el nodo en esta posicion.
+    def insertar_final(self, item):
+        nuevo_nodo = Nodo.Nodo(item) #Este es el nuevo nodo
+        if self.inicio is None:
+            self.inicio = nuevo_nodo #En caso de estar vacia directamente se inserta al inicio
+        else:
+            puntero = self.inicio #De otro modo, buscara el final de la lista viendo si tiene un nodo
+            while puntero.next is not None:
+                puntero = puntero.next
+            puntero.next = nuevo_nodo #Luego inserta el nodo en el puntero donde se encuentra el final
+
+    def insertar_posicion(self, item, pos):
+        nuevo_nodo = Nodo.Nodo(item) #Crea el nodo
+        if self.inicio is None or pos == 1:
+            #Para no tener que buscar durante toda la lista, lo mismo si la posicion
+            # es la primera directamente se inserta en esta posicion
+            nuevo_nodo.next = self.inicio
+            self.inicio = nuevo_nodo
+        else: #De otro modo se busca de igual forma donde esta la posicion
+            puntero = self.inicio
+            for _ in range(pos - 2):
+                if puntero.next is None:
+                    break
+                puntero = puntero.next
+            nuevo_nodo.next = puntero.next #Y se agrega el nuevo nodo
+            puntero.next = nuevo_nodo
+
+    def eliminar_inicio(self): #Se elimina el nodo que se encuentra al inicio de la lista
+        if self.inicio is None:
+            print("La lista esta vacia!")
+        elif self.inicio.next is None: #En este caso, si solo hay un elemento simplemente elimina el unico nodo presente
+            self.inicio = None
+        else: #De otra forma, busco el primer nodo
+            puntero_ant = self.inicio
+            puntero_sig = self.inicio.next
+            while puntero_sig.next is not None:
+                puntero_ant = puntero_sig
+                puntero_sig = puntero_sig.next
+            puntero_ant.next = None
+
+    def eliminar_final(self): #Se elimina el nodo que se encuentra al final de la lista xd
+        if self.inicio is None:
+            print("La lista está vacía!")
+        elif self.inicio.next is None: #Misma metodologia que el anterior
+            self.inicio = None
+        else:
+            puntero_ant = self.inicio
+            puntero_sig = self.inicio.next
+            while puntero_sig.next is not None:
+                puntero_ant = puntero_sig
+                puntero_sig = puntero_sig.next
+            puntero_ant.next = None
+
+    def eliminar_posicion(self, pos): #Buscamos en una posicion dentro de la lista y lo elimina
+        if self.inicio is None:
+            print("La lista está vacía!")
+            return
+        if pos == 1:
+            self.inicio = self.inicio.next
+            return
+        puntero = self.inicio
+        for _ in range(pos - 2):
+            if puntero.next is None:
+                print("Posición fuera de rango")
+                return
+            puntero = puntero.next
+        if puntero.next is None:
+            print("Posición fuera de rango")
+        else:
+            puntero.next = puntero.next.next
+
+    def mostrar(self):
+        #Lista todos los elementos dentro de la lista, y el final es un null, como se define en este tipo de listas
+        if self.inicio is None:
+            print("La lista está vacía")
+        else:
+            puntero = self.inicio
+            while puntero is not None:
+                print(f"{puntero.dato} -> ", end="")
+                puntero = puntero.next
+            print("None")
+
+    #Variables usadas:
+    #Siempre instanciaba un nuevo nodo en cada metodo.
+    #Las variables como puntero son para buscar dentro de la lista, puntero anterior y siguiente
+    #es para mantener esa data a la hora de eliminar en una posicion especifica
+    #En general, es una implementacion sencilla de las listas simplemente enlazadas
---
+++ main.py
@@ -0,0 +1,54 @@
+import Lista
+import os
+clear = lambda: os.system('cls')
+if __name__ == "__main__":
+    print("Bienvenido a mi implementación de Lista Enlazada en Python")
+    lista = Lista.List()
+    lista.insertar_inicio(1)
+    lista.insertar_inicio("arroz")
+    lista.insertar_inicio(3.14153213)
+    lista.insertar_inicio("implementado")
+    #Agrege un menu funcional para que pueda ser usado por consola.
+    #Ademas, inicialize algunos datos en la list para poder visualizarlos al ejecutar
+    running = True
+    while running:
+        print("\n############ MENU ############\n")
+        print("1. Ver la Lista")
+        print("2. Agregar elemento al inicio de la lista")
+        print("3. Agregar elemento al final de la lista")
+        print("4. Agregar elemento a una posicion objetivo de la lista")
+        print("5. Eliminar elemento al inicio de la lista")
+        print("6. Eliminar elemento al final de la lista")
+        print("7. Eliminar elemento a una posicion objetivo de la lista")
+        print("8. Salir del programa")
+        try:
+            opc = int(input())
+            if opc == 1:
+                clear()
+                lista.mostrar()
+            elif opc == 2:
+                clear()
+                lista.insertar_inicio(input("Insertar al inicio de la lista: "))
+            elif opc == 3:
+                clear()
+                lista.insertar_final(input("Insertar al final de la lista: "))
+            elif opc == 4:
+                clear()
+                lista.mostrar()
+                lista.insertar_posicion(input("Insertar al final de la lista: "),int(input("Insertar posicion objetivo de la lista: ")))
+            elif opc == 5:
+                clear()
+                lista.eliminar_inicio()
+            elif opc == 6:
+                clear()
+                lista.eliminar_final()
+            elif opc == 7:
+                clear()
+                lista.mostrar()
+                lista.eliminar_posicion(int(input("Insertar posicion objetivo de la lista: ")))
+            elif opc == 8:
+                running = False
+            else:
+                print("Opcion no valida, ingrese otra")
+        except ValueError:
+            print("El numero ingresado no es valido")
